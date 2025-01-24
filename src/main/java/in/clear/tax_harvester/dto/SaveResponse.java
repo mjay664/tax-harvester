c90be13881a862b06395b46f9d9ad53380f2c3f3
mjay664 <jay.raj@clear.in>
2025-01-24 06:11:17 +0530

    new change

--- src/main/java/in/clear/tax_harvester/dto/SaveResponse.java
+++ src/main/java/in/clear/tax_harvester/dto/SaveResponse.java
@@ -4,6 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.util.CollectionUtils;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Data
 @Builder
@@ -11,4 +18,56 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class SaveResponse {
     private InvestmentTransactionResponseV2DTO response;
+
+    public FolioDataResponse toFolioDataResponse() {
+        var transactions = response.getTransactions();
+        var fundFolioDataList = consolidateTransactions(transactions);
+        return new FolioDataResponse(fundFolioDataList);
+    }
+
+    private List<FundFolioData> consolidateTransactions(List<MutualFundTransactionDTO> transactions) {
+        if (CollectionUtils.isEmpty(transactions)) {
+            return List.of();
+        }
+
+        Map<Product, List<MutualFundTransactionDTO>> transactionMap = transactions.stream()
+                                                                                  .collect(Collectors.groupingBy(MutualFundTransactionDTO::getProduct));
+        var fundFolioDataList = new ArrayList<FundFolioData>();
+        for(var entry: transactionMap.entrySet()) {
+            if (entry.getValue().isEmpty()) {
+                continue;
+            }
+
+            fundFolioDataList.add(createFundFolioData(entry.getKey(), entry.getValue()));
+        }
+
+
+        return fundFolioDataList;
+    }
+
+    private static FundFolioData createFundFolioData(Product product, List<MutualFundTransactionDTO> mutualFundTransactionDTOS) {
+        var fundProduct = product.getReferProduct();
+
+        var fundFolioData = new FundFolioData();
+
+        var fundTransactions = mutualFundTransactionDTOS.stream()
+                                                        .map(MutualFundTransactionDTO::toFolioTransactionData)
+                                                        .toList();
+
+        fundFolioData.setFolioTransactionDataList(new ArrayList<>(fundTransactions));
+        fundFolioData.setFundName(fundProduct.getFundName());
+        fundFolioData.setIsinCode(fundProduct.getIsinCode());
+        fundFolioData.setInvestedAmount(fundTransactions.stream()
+                                                        .map(FolioTransactionData::getInvestedAmount)
+                                                        .reduce(BigDecimal.ZERO, BigDecimal::add));
+        fundFolioData.setCurrentAmount(fundTransactions.stream()
+                                                       .map(FolioTransactionData::getCurrentAmount)
+                                                       .reduce(BigDecimal.ZERO, BigDecimal::add));
+        fundFolioData.setUnits(fundTransactions.stream().map(FolioTransactionData::getUnits)
+                                               .reduce(BigDecimal.ZERO, BigDecimal::add));
+        fundFolioData.setFundType(fundProduct.getFundType());
+        return fundFolioData;
+    }
+
+
 }
